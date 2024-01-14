6ab66aa7e8479a7ec6b2417aaceda278199f46bd
Schnnuu <80274118+Schnnuu@users.noreply.github.com>
2024-01-14 10:44:28 +0100

    Added Student.java and StudentTest.java

--- src/Student.java
+++ src/Student.java
@@ -1,26 +1,58 @@
-public class Student implements StudentInterface{
+public class Student extends BinTreeGen implements StudentInterface{
+    private int matrikelNummer;
+    private String name;
+    private String course;
+    public Student(){
+
+    }
+    /**
+     * a unique 6-digits matriculation number is assigned to each student
+     * upon enrollment
+     * each student has a name
+     * each student has a course he/she has enrolled for
+     *
+     * @param m matriculation number, must be greater than or equal to 100000
+     * @param n name, cannot be null or empty string
+     * @param c course, cannot be null or empty string
+     * @throws IllegalArgumentException if matriculationNo, name or course is
+     *                                  not valid
+     * @throws NullPointerException     if name or course is null
+     */
+    public Student(int m, String n, String c)
+            throws IllegalArgumentException, NullPointerException {
+        if (find(m) != null) throw new IllegalArgumentException("Matrikelnummer ist bereits vergeben");
+        matrikelNummer = m;
+        name = n;
+        course = c;
+
+    }
+
     @Override
     public int getMatriculationNo() {
-        return 0;
+        return matrikelNummer;
     }
 
     @Override
     public String getName() {
-        return null;
+        return name;
     }
 
     @Override
     public void setName(String name) throws NullPointerException, IllegalArgumentException {
-
+        if (name == null)throw new NullPointerException("Name can't be null");
+        if (name.equals("")) throw new IllegalArgumentException("Name can't be empty");
+        this.name = name;
     }
 
     @Override
     public String getCourse() {
-        return null;
+        return course;
     }
 
     @Override
     public void setCourse(String course) throws NullPointerException, IllegalArgumentException {
-
+    if (course == null) throw new NullPointerException("Course can't be null");
+    if(course.equals(""))throw new IllegalArgumentException("Course can't be empty");
+    this.course = course;
     }
 }
---
+++ src/StudentTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentTest {
+
+    @Test
+    void getMatriculationNo() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void getCourse() {
+    }
+
+    @Test
+    void setCourse() {
+    }
+}
